db0b679dbeac9834dd0f8ddf1eb9381796578e6e
Anderson <anderson.consultant.it@outlook.com>
2021-09-24 20:44:22 -0400

    chore: included react-refresh-webpack-plugin and reach-refresh packages
    This configs prevents the app from losing the current state after saving changes in a file with webpack server running

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
@@ -15,19 +15,28 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
     devServer: {
-        static: path.resolve(__dirname, 'public')
+        static: path.resolve(__dirname, 'public'),
+        hot: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ],
+        }),
+        isDev && new ReactRefreshWebpackPlugin()
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDev && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
